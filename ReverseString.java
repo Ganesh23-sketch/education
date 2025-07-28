e6d7be7d7b9dfc8fc8c302e80cbc92c80107cf64
Ganesh23-sketch <ganeshjaikar01@gmail.com>
2025-07-28 22:44:29 +0530

    reverse string program

---
+++ ReverseString.java
@@ -0,0 +1,10 @@
+public class ReverseString{
+    public static void main(String[] args) {
+        String name ="Ganesh";
+        String reverse="";
+        for(int i=0;i<name.length();i++){
+            reverse=name.charAt(i) + reverse;
+        }
+        System.out.println(reverse);
+    }
+}
