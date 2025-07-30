952d90a75d10359221e4bcf9bff5eae63b83b368
Ganesh Jaikar <ganeshjaikar01@gmail.com>
2025-07-30 10:09:38 +0530

    Add files via upload

---
+++ If.java
@@ -0,0 +1,14 @@
+public class If{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        if(a+b<=20){
+            System.out.println("a+b is grether than 20");
+        }
+        else {    
+        System.out.println("a+b is less than 20");
+
+        }    
+
+    }
+}
